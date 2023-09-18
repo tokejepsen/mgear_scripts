9025c84fb03e924dce329d04a7b0ff969eac13a8
Toke Stuart Jepsen <tokejepsen@gmail.com>
2023-09-18 15:19:40 +0100

    Fix deltamush importer

--- post_script_deltamush_import.py
+++ post_script_deltamush_import.py
@@ -9,12 +9,13 @@ directory = os.path.dirname(pymel.core.sceneName())
 
 # Find *.deltamush folder starting with same name as current file
 json_files = []
-for folder in os.listdir(directory):
-    if folder == (filename + ".deltamush"):
-        path = os.path.join(directory, folder)
-        for f in os.listdir(path):
-            if f.endswith(".json"):
-                json_files.append(os.path.join(path, f))
+if os.path.exists(directory):
+    for folder in os.listdir(directory):
+        if folder == (filename + ".deltamush"):
+            path = os.path.join(directory, folder)
+            for f in os.listdir(path):
+                if f.endswith(".json"):
+                    json_files.append(os.path.join(path, f))
 
 for json_file in json_files:
     mesh_name = os.path.splitext(os.path.basename(json_file))[0]
