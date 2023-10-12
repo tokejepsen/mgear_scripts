0a7c1e011fdfc2ba80545446a287885102c1dd8a
Toke Stuart Jepsen <tokejepsen@gmail.com>
2023-10-12 18:13:41 +0100

    Fix ngskintools import

--- post_script_ngskintools_import.py
+++ post_script_ngskintools_import.py
@@ -1,9 +1,13 @@
 import os
 
 import pymel.core as pc
-from ngSkinTools2.api import InfluenceMappingConfig, VertexTransferMode
+from ngSkinTools2 import api
 import mgear
 
+
+config = api.InfluenceMappingConfig()
+config.use_distance_matching = True
+config.use_name_matching = False
 
 basename = os.path.basename(pc.sceneName())
 filename = os.path.splitext(basename)[0]
@@ -21,29 +25,36 @@ mismatch_files = []
 targets = []
 for json_file in json_files:
     path = os.path.join(directory, json_file)
-    importer = ngSkinTools.importExport.JsonImporter()
-    with open(path) as f:
-        data = importer.process(f.read())
+    mesh_name = os.path.splitext(os.path.basename(json_file))[0]
 
-        # Setup skinned mesh from influences in file
-        joints = [pc.PyNode(x) for x in data.getAllInfluences()]
+    importer = api.transfer.LayersTransfer()
+    importer.vertex_transfer_mode = (
+        api.transfer.VertexTransferMode.closestPoint
+    )
+    importer.influences_mapping.config = config
+    importer.load_source_from_file(path, "json")
+    importer.target = mesh_name
+    joints = []
+    for influence_info in importer.influences_mapping.influences:
+        joints.append(influence_info.path)
+        if not pc.objExists(influence_info.path):
+            print("Missing " + influence_info.path)
 
-        # Find mesh from filename
-        mesh_name = os.path.splitext(os.path.basename(json_file))[0]
-        print(mesh_name)
-        if not pc.objExists(mesh_name):
-            mismatch_files.append(json_file)
-            continue
-        mesh = pc.PyNode(mesh_name)
-        targets.append(mesh)
+    # Find mesh from filename
+    print(mesh_name)
+    if not pc.objExists(mesh_name):
+        mismatch_files.append(json_file)
+        continue
+    mesh = pc.PyNode(mesh_name)
+    targets.append(mesh)
 
-        pc.select(clear=True)
-        pc.select(sorted(joints), mesh)
-        skin_cluster = pc.skinCluster(
-            toSelectedBones=True, skinMethod=2, removeUnusedInfluence=False
-        )
-        pc.rename(skin_cluster, "{}_skin_cluster".format(mesh_name))
-        data.saveTo(mesh.name())
+    pc.select(clear=True)
+    pc.select(sorted(joints), mesh)
+    skin_cluster = pc.skinCluster(
+        toSelectedBones=True, skinMethod=2, removeUnusedInfluence=False
+    )
+    pc.rename(skin_cluster, "{}_skin_cluster".format(mesh_name))
+    importer.execute()
 
 if mismatch_files:
     mgear.log(
