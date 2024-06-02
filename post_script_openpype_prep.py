f77cbc19ceee32950abe1fb3c5f13a920b787186
Toke Stuart Jepsen <tokejepsen@gmail.com>
2024-06-02 14:30:56 +0100

    Account for exclude controls

--- post_script_openpype_prep.py
+++ post_script_openpype_prep.py
@@ -1,3 +1,6 @@
+import os
+import json
+
 import pymel.core as pc
 
 
@@ -14,10 +17,29 @@ else:
     pc.select(clear=True)
     controls_set = pc.sets(name="rigMain_controls_SET")
 
-controls_set.addMembers(pc.PyNode("rig_controllers_grp").members())
-pc.PyNode("rigMain").addMembers([controls_set])
 
-# Remove control buffer shapes.
-for member in controls_set.members():
-    if member.name().endswith("controlBuffer"):
-        controls_set.remove(member)
+basename = os.path.basename(pc.sceneName())
+filename = os.path.splitext(basename)[0]
+directory = os.path.dirname(pc.sceneName())
+
+# Find "{current file name}/parents.json"
+exclude_set = None
+if pc.objExists("exclude_controls"):
+    exclude_set = pc.PyNode("exclude_controls")
+else:
+    pc.select(clear=True)
+    exclude_set = pc.sets(name="exclude_controls")
+
+path = os.path.join(directory, filename, "exclude_controls.json")
+controls = pc.PyNode("rig_controllers_grp").members()
+if os.path.exists(path):
+    with open(path) as f:
+        for node in json.load(f):
+            if node not in controls:
+                continue
+
+            controls.remove(node)
+            exclude_set.addMembers([node])
+
+controls_set.addMembers(controls)
+pc.PyNode("rigMain").addMembers([controls_set])
