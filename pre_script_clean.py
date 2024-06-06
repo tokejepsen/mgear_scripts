3f02b343a69954c2edc34c04355277012a182342
Toke Stuart Jepsen <tokejepsen@gmail.com>
2024-06-06 17:08:01 +0100

    Clean up sets before building.

--- pre_script_clean.py
+++ pre_script_clean.py
@@ -4,3 +4,11 @@ import pymel.core as pm
 pm.delete(pm.ls(type="ngSkinLayerData"))
 # Delete all controller tags
 pm.delete(pm.ls(type="controller"))
+
+# Nodes to ensure are deleted.
+nodes = ["rig_sets_grp", "rig_controllers_grp", "rig_deformers_grp"]
+for node in nodes:
+    if not pm.objExists(node):
+        continue
+
+    pm.delete(node)
